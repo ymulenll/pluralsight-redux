70087eedf9088c0b4c3d50d92a47be870b0c62c7
Yoelvis Mulen <ymulenll@gmail.com>
2018-08-14 00:06:43 -0300

    Implemented async writes using redux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,62 @@
 import * as types from './actionTypes';
+import coursesApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSE_SUCCESS, courses };
 }
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export const loadCourses = () => dispatch => {
+  coursesApi.getAllCourses()
+    .then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    })
+    .catch(error => {
+      throw (error);
+    });
+};
+
+// export function loadCourses() {
+//   return function (dispatch) {
+//     return coursesApi.getAllCourses()
+//       .then(courses => {
+//         dispatch(loadCoursesSuccess(courses));
+//       })
+//       .catch(error => {
+//         throw (error);
+//       });
+//   };
+// }
+
+export const saveCourse = course => dispatch => {
+  coursesApi.saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw (error);
+      });
+};
+
+// export function saveCourse(course) {
+//   return function (dispatch) {
+//     return coursesApi.saveCourse(course)
+//       .then(savedCourse => {
+//         course.id
+//           ? dispatch(updateCourseSuccess(saveCourse))
+//           : dispatch(createCourseSuccess(savedCourse));
+//       })
+//       .catch(error => {
+//         throw (error);
+//       });
+//   };
+// }
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,51 +1,35 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as courseActions from '../../actions/courseActions';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as courseActions from "../../actions/courseActions";
+import PropTypes from "prop-types";
+import CourseList from "./CourseList";
+import { browserHistory } from "react-router";
 
 class CoursesPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      course: { title: '' }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course });
-  }
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
+  redirectToAddCoursePage() {
+    browserHistory.push("/course");
   }
 
   render() {
+    const { courses } = this.props;
     return (
       <div>
         <h1>Courses</h1>
-          {this.props.courses.map(this.courseRow)}
-
-        <h2>Add course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
-
         <input
           type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}
+        />
+
+        <CourseList courses={courses} />
       </div>
     );
   }
@@ -68,4 +52,7 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
